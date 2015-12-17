d61acece3ecf05c084e874c2fc96077aa3416dac
Ryan Kenney <rkenney@moesol.com>
2015-12-16 18:58:02 -0800

    Tweaked usage and added README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+git-tool.jar
+====================
+
+This is a basic java application used to manipulate git repositories.
+
+Usage
+--------------------
+
+```
+  java git-tool.jar [fix-urls [--simulate] <base-directory>]%n", App.class.getName());
+
+  fix-urls
+    Upates the remote urls of moesol repos from the old format to the new (Dec 2015).
+  --simulate
+    Simulates execution without any side-effects.
+  base-directory
+    The base directory to search in. This may contain any number of git repos nested at any depth.
+
+```
+
+The following command causes the tool to search through all subdirectories of "my/files", identify git repos with "remote" urls points to the Moebius Solutions webserver in the old url format, and updates those urls.
+
+```
+java git-tool.jar fix-urls my/files
+```
+
+Adding the "--simulate" option causes the fixes to be identified, but not executed.
+
+```
+java git-tool.jar fix-urls --simulate my/files
+```
--- src/main/java/com/github/MoebiusSolutions/git_tool/App.java
+++ src/main/java/com/github/MoebiusSolutions/git_tool/App.java
@@ -12,24 +12,51 @@ public class App {
 	
     public static void main( String[] args ) throws Exception
     {
-    	boolean doSimulate = false;
-
-    	// Parse args
     	LinkedList<String> argList = new LinkedList<String>(Arrays.asList(args));
-    	while (argList.size() > 1) {
+    	if (argList.size() < 1) {
+    		printUsageAndTerminate();
+    	}
+    	
+    	// Process args
+    	while (argList.size() > 0) {
     		String arg = argList.removeFirst();
-    		if ("--simulate".equals(arg)) {
-    			doSimulate = true;
+    		
+    		// "fix-urls"
+    		if ("fix-urls".equals(arg)) {
+    	    	boolean doSimulate = false;
+    			// Process params to "fix-urls"
+    			while (argList.size() > 1) {
+    				String param = getRequiredArg(argList);
+    				switch (param) {
+    				case "--simulate":
+    					doSimulate = true;
+    					continue;
+					default:
+						printUsageAndTerminate();
+    				}
+    			}
+    			String basePath = getRequiredArg(argList);
+    			fixUrls(Paths.get(basePath), doSimulate);
     			continue;
     		}
+    		
     		// Unrecognized argument
     		printUsageAndTerminate();
     	}
-    	if (argList.size() != 1) {
+    }
+    
+    /**
+	 * Terminates the program and prints usage if an arg is not available in the
+	 * provided list. Removes the first arg and returns it if one is available.
+	 */
+    private static String getRequiredArg(LinkedList<String> argList) {
+    	if (argList.size() < 1) {
     		printUsageAndTerminate();
     	}
-    	Path baseDir = Paths.get(argList.removeFirst());
+    	return argList.removeFirst();
+    }
 
+    private static void fixUrls(Path baseDir, boolean doSimulate) {
     	if (doSimulate) {
 	    	System.out.println("");
 	    	System.out.println("*** Executing without Side Effects (--simulate) **");
@@ -48,20 +75,28 @@ public class App {
         	processor.handle(new GitRemoteTty(repoDir, tty));
     	}
     }
-    
-    private static List<Path> findGitDirs(Path baseDir) throws IOException {
-    	GitDirFinder gitDirs = new GitDirFinder();
-    	Files.walkFileTree(baseDir, gitDirs);
-    	return gitDirs.getRepoDirs();
+
+    private static List<Path> findGitDirs(Path baseDir) {
+    	try {
+	    	GitDirFinder gitDirs = new GitDirFinder();
+	    	Files.walkFileTree(baseDir, gitDirs);
+	    	return gitDirs.getRepoDirs();
+    	} catch (IOException e) {
+    		throw new RuntimeException(e);
+    	}
     }
 
 	private static void printUsageAndTerminate() {
 		System.out.printf("%n");
 		System.out.printf("Usage: %n");
-		System.out.printf("  java git-tool.jar [--simulate] <base-directory>%n", App.class.getName());
+		System.out.printf("  java git-tool.jar [fix-urls [--simulate] <base-directory>]%n", App.class.getName());
 		System.out.printf("%n");
+		System.out.printf("  fix-urls%n");
+		System.out.printf("    Upates the remote urls of moesol repos from the old format to the new (Dec 2015).%n");
 		System.out.printf("  --simulate%n");
 		System.out.printf("    Simulates execution without any side-effects.%n");
+		System.out.printf("  base-directory%n");
+		System.out.printf("    The base directory to search in. This may contain any number of git repos nested at any depth.%n");
 		System.out.printf("%n");
 		System.exit(1);
 	}
